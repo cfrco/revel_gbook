5e824072dcc7399376c1eccde1ebc83f06ba7b26
CatCfrco <z82206.cat@gmail.com>
2014-01-02 11:14:47 +0800

    edit/remove

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -5,26 +5,28 @@ import "labix.org/v2/mgo"
 import "labix.org/v2/mgo/bson"
 import "gbook/app/models"
 import "github.com/cfrco/mon"
-import "fmt"
+//import "fmt"
 
 type App struct {
     *revel.Controller
+    DBsession *mgo.Session
 }
 
-func (c App) Index() revel.Result {
+func (c *App) connectDB() {
     session, err := mgo.Dial("localhost")
     if err != nil {
         panic(err)
     }
-    defer session.Close()
+    c.DBsession = session
 
     // Optional. Switch the session to a monotonic behavior.
-    session.SetMode(mgo.Monotonic, true)
-    
-    co := session.DB("SE_test").C("message")
-    if err != nil {
-        panic(err)
-    }
+    c.DBsession.SetMode(mgo.Monotonic, true)
+}
+
+func (c App) Index() revel.Result {
+    c.connectDB()
+    defer c.DBsession.Close()
+    co := c.DBsession.DB("SE_test").C("message")
 
     var messages []models.Message
     mon.QueryAll(co,co.Find(bson.M{}),&messages)
@@ -32,31 +34,52 @@ func (c App) Index() revel.Result {
     return c.Render(messages)
 }
 
-func (c App) New(name string) revel.Result {
-    session, err := mgo.Dial("localhost")
-    if err != nil {
-        panic(err)
-    }
-    defer session.Close()
-
-    // Optional. Switch the session to a monotonic behavior.
-    session.SetMode(mgo.Monotonic, true)
-    
-    co := session.DB("SE_test").C("message")
-    if err != nil {
-        panic(err)
-    }
+func (c App) New(name string, mail string, content string) revel.Result {
+    c.connectDB()
+    defer c.DBsession.Close()
+    co := c.DBsession.DB("SE_test").C("message")
     
     method := c.Controller.Params.Values["METHOD"][0]
     if method == "GET" {
     } else if method == "POST" {
         //fmt.Println(c.Controller.Params.Values)
-        message := models.NewMessage(co,
-            c.Controller.Params.Values["name"][0],
-            c.Controller.Params.Values["mail"][0],
-            c.Controller.Params.Values["content"][0])
+        message := models.NewMessage(co, name, mail, content)
         message.Insert()
         return c.Redirect("/")
     }
     return c.Render()
+}
+
+func (c App) Edit(mid string) revel.Result {
+    c.connectDB()
+    defer c.DBsession.Close()
+    co := c.DBsession.DB("SE_test").C("message")
+
+    message := models.NewMessage(co, "", "", "")
+    message.Find(bson.M{"_id":bson.ObjectIdHex(mid)})
+
+    method := c.Controller.Params.Values["METHOD"][0]
+    if method == "GET" {
+    } else if method == "POST" {
+        message.AuthorName = c.Controller.Params.Values["name"][0]
+        message.AuthorMail = c.Controller.Params.Values["mail"][0]
+        message.Content = c.Controller.Params.Values["content"][0]
+        message.Update()
+        return c.Redirect("/")
+    }
+
+    return c.Render(message)
+}
+
+func (c App) Remove(mid string) revel.Result {
+    c.connectDB()
+    defer c.DBsession.Close()
+    co := c.DBsession.DB("SE_test").C("message")
+
+    message := models.NewMessage(co, "", "", "")
+    message.Find(bson.M{"_id":bson.ObjectIdHex(mid)})
+
+    message.Remove()
+
+    return c.Redirect("/")
 }
